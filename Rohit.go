3e32f5bf3af3f6453a5dbf900c82963398d68bc8
rohit roshan jena <rohit.r@iserveu.co.in>
2025-03-11 12:10:13 +0530

    modified:   Rohit.go

--- Rohit.go
+++ Rohit.go
@@ -4,20 +4,26 @@ import "fmt"
 
 // Add adds two integers and returns the result.
 func Add(a, b int) int {
+	checkName()
 	return a + b
 }
 
 // Multiply multiplies two integers and returns the result.
 func Multiply(a, b int) int {
+	checkName()
 	return a * b
 }
 
 var name string
 func Rohit(s string)  {
+	checkName()
+	fmt.Println(name," Says",s)	
+}
+
+func checkName(){
 	if name == "" {
 		panic("Plz set name")
 	}
-	fmt.Println(name," Says",s)	
 }
 
 func SetName(s string){
