8a024d1c8aec44e9685ed6d5c634c77c896af957
rohit roshan jena <rohit.r@iserveu.co.in>
2025-03-11 11:54:45 +0530

    qwerty

---
+++ Rohit.go
@@ -0,0 +1,17 @@
+package mathutils
+
+import "fmt"
+
+// Add adds two integers and returns the result.
+func Add(a, b int) int {
+	return a + b
+}
+
+// Multiply multiplies two integers and returns the result.
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func Rohit(s string)  {
+	fmt.Println("Rohit Says",s)	
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/00RohitRoshan/Rohit
+
+go 1.23.4
